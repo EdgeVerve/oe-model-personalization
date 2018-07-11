7b3d61dac741be06a93dc692910a106d69722a11
Atul <atul_pandit@edgeverve.com>
2018-07-11 17:14:14 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,10 +57,10 @@ module.exports = function GruntConfig(grunt) {
         options: {
           timeout: 60000,
           check: {
-            lines: 10,
-            statements: 10,
-            branches: 1,
-            functions: 1
+            lines: 80,
+            statements: 80,
+            branches: 60,
+            functions: 60
           },
           reportFormats: ['lcov']
         }
