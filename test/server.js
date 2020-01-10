93003966924cc7c44dcf7f6e9d814bf82785786b
vamsee <vamseerama_c@infosys.com>
2020-01-10 16:51:38 +0530

    removed unneeded packages and updated ci

--- test/server.js
+++ test/server.js
@@ -7,14 +7,14 @@
 
 // Author : Atul
 var oecloud = require('oe-cloud');
-var loopback=require('loopback');
+var loopback = require('loopback');
 
 oecloud.observe('loaded', function (ctx, next) {
-  oecloud.setBaseEntityAutoscope(["tenantId"]);
-  oecloud.attachMixinsToBaseEntity("ModelPersonalizationMixin");
-  oecloud.setModelDefinitionAutoscope(["tenantId"]);
+  oecloud.setBaseEntityAutoscope(['tenantId']);
+  oecloud.attachMixinsToBaseEntity('ModelPersonalizationMixin');
+  oecloud.setModelDefinitionAutoscope(['tenantId']);
   return next();
-})
+});
 
 oecloud.boot(__dirname, function (err) {
   oecloud.start();
